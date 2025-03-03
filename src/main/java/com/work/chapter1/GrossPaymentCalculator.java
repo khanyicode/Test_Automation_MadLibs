cfd0b6cccc8989b842b5c7bbd119f44141a24dc4
khanyisile <141396685+khanyicode@users.noreply.github.com>
2025-03-03 11:29:09 +0200

    questions added

---
+++ src/main/java/com/work/chapter1/GrossPaymentCalculator.java
@@ -0,0 +1,15 @@
+//Gross Pay Calculator Problem
+
+//Write a Java program that calculates an employee's gross pay based on the number of hours worked and their hourly pay rate.
+//Instructions:
+
+   // Ask the user to enter the number of hours the employee worked.
+   // Ask the user to enter the employee’s hourly pay rate.
+    //Multiply the number of hours worked by the hourly pay rate to calculate the gross pay.
+    //Display the employee’s gross pay in a user-friendly format.
+
+package main.java.com.work.chapter1;
+
+public class GrossPaymentCalculator {
+    
+}
