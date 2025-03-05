faf38facfa0ee2448f64f465aab8fb2fa36d7283
khanyisile <141396685+khanyicode@users.noreply.github.com>
2025-03-05 10:57:07 +0200

    completed the GrossPayinputValidation

--- src/main/java/com/work/chapter4/GrossPayInputValidation.java
+++ src/main/java/com/work/chapter4/GrossPayInputValidation.java
@@ -1,3 +1,41 @@
+/* Question:  
+Write a Java program that prompts the user to:  
+
+    1. Enter the number of hours worked for the week.  
+    2. Validate that the entered hours are between 1 and 40 (inclusive).  
+    3. If the input is invalid, prompt the user to enter a valid number again.  
+    4. Calculate the employee's gross pay based on an hourly rate of $15.  
+    5. Display the gross pay to the user. */
+
+import java.util.Scanner;
+
 public class GrossPayInputValidation {
+
+    public static void main(String[] args) {
+
+          int hourrate= 15;
+
+          int maximumhours = 40;
+
+
+        System.out.println("Enter the number of hours worked for the week. ");
+        Scanner scanner = new Scanner(System.in);
+        double employeeanswer = scanner.nextDouble();
+
+        while( employeeanswer > maximumhours){
+            System.out.println("Invalid entry. Your hours must be between 1 and 40. Try again.\"");
+            double hoursworked = scanner.nextDouble();
+        }
+
+        scanner.close();
+
+        double gross = employeeanswer * hourrate;
+
+        System.out.println(" you worked "+ gross);
+
+       
+    }
+
+
     
 }
