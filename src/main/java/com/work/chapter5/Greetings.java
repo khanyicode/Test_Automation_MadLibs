2324dc486cf1ceefdb871fc2db80f97812de59b4
khanyisile <141396685+khanyicode@users.noreply.github.com>
2025-03-17 17:01:13 +0200

    aded chapter 5

---
+++ src/main/java/com/work/chapter5/Greetings.java
@@ -0,0 +1,4 @@
+/*
+ * OUR FIRST METHOD
+ * Write a method that asks a user for their name, then greets them by name.
+ */
---
+++ src/main/java/com/work/chapter5/InstantCreditCheck.java
@@ -0,0 +1,9 @@
+
+/*
+ * VARIABLE SCOPE
+ * Write an ‘instant credit check’ program that approves
+ * anyone who makes more than $25,000 and has a credit score
+ * of 700 or better. Reject all others.
+ */
+
+ 
