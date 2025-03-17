b7a6fb7cd7fe98be1cc7d1dca2626f9ece032eac
khanyisile <141396685+khanyicode@users.noreply.github.com>
2025-03-17 16:31:42 +0200

    added the question for Letterssearch

--- src/main/java/com/work/chapter4/LetterSearch.java
+++ src/main/java/com/work/chapter4/LetterSearch.java
@@ -1,3 +1,18 @@
+
+/*- Question
+
+Write a Java program that searches a given text to determine if it contains the letter **'A'** (case insensitive).  
+The program should stop searching as soon as it finds the letter.
+
+Task:
+
+1. Prompt the user to enter a string.  
+2. Use a **for loop** to iterate through each character in the string.  
+3. If the letter 'A' (uppercase or lowercase) is found, set a flag to `true` and exit the loop using `break`.  
+4. Display an appropriate message indicating whether the letter 'A' was found.*/
+
+
+
 public class LetterSearch {
     
 }
